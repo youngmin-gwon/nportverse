5fbab825d0421fa9b8a26c14233a7599f86cd24e
Youngmin Gwon <youngmin-gwon@users.noreply.github.com>
2023-09-01 08:54:33 +0900

    👔 Chore: Update routing logic

--- lib/src/core/presentation/router/main_app_router_delegate.dart
+++ lib/src/core/presentation/router/main_app_router_delegate.dart
@@ -2,6 +2,9 @@ import 'package:crypto_concept/src/core/presentation/router/main_app_configurati
 import 'package:crypto_concept/src/core/presentation/router/pages.dart';
 import 'package:flutter/material.dart';
 
+/// OS 로 화면을 변경하라는 명령을 받아 이를 어느 화면으로 가야할지 처리하는 class
+///
+/// 현재는 간단하게 [HomeScreen] 으로만 갈 수 있게 처리 되어있음
 class MainAppRouterDelegate extends RouterDelegate<MainAppConfiguration>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin {
   MainAppRouterDelegate() : _navigatorKey = GlobalKey();
@@ -16,8 +19,7 @@ class MainAppRouterDelegate extends RouterDelegate<MainAppConfiguration>
     return Navigator(
       key: _navigatorKey,
       pages: [
-        // StartPage(),
-        GameIntroPage(),
+        HomePage(),
       ],
       onPopPage: (route, result) {
         if (!route.didPop(result)) return false;
